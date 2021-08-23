75e71bccbedcdbd5f8791c34acae39450906c84a
Robson Ferreira <robson-ferreira@hotmail.com>
2021-08-23 02:59:21 -0300

    initializing migration on application startup

--- migrations/20210821020436-OpenMarket.js
+++ migrations/20210821020436-OpenMarket.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     /**
@@ -8,42 +6,42 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
-     await queryInterface.createTable('open_markets', { 
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true,
-			allowNull: false
-		},
-		external_pk: Sequelize.INTEGER,
-		name: Sequelize.STRING,
-		longitude: Sequelize.STRING,
-		latitude: Sequelize.STRING,
-		setcens: Sequelize.STRING,
-		areap: Sequelize.STRING,
-		cod_dist: Sequelize.STRING,
-		district: Sequelize.STRING,
-		cod_sub_pref: Sequelize.STRING,
-		sub_prefe: Sequelize.STRING,
-		region5: Sequelize.STRING,
-		region8: Sequelize.STRING,
-		record: Sequelize.STRING,
-		public_place: Sequelize.STRING,
-		number: Sequelize.STRING,
-		neighborhood: Sequelize.STRING,
-		reference: Sequelize.STRING,
-		created_at: Sequelize.DATE,
-		updated_at: Sequelize.DATE,
-	});
+    await queryInterface.createTable('open_markets', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      external_pk: Sequelize.INTEGER,
+      name: Sequelize.STRING,
+      longitude: Sequelize.STRING,
+      latitude: Sequelize.STRING,
+      setcens: Sequelize.STRING,
+      areap: Sequelize.STRING,
+      cod_dist: Sequelize.STRING,
+      district: Sequelize.STRING,
+      cod_sub_pref: Sequelize.STRING,
+      sub_prefe: Sequelize.STRING,
+      region5: Sequelize.STRING,
+      region8: Sequelize.STRING,
+      record: Sequelize.STRING,
+      public_place: Sequelize.STRING,
+      number: Sequelize.STRING,
+      neighborhood: Sequelize.STRING,
+      reference: Sequelize.STRING,
+      created_at: Sequelize.DATE,
+      updated_at: Sequelize.DATE,
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-     await queryInterface.dropTable('coins');
-  }
+    await queryInterface.dropTable('coins');
+  },
 };
